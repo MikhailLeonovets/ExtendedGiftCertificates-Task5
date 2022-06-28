b376bca5084b98002afa8031fab9f43ca2a2018a
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-06-28 14:49:22 +0300

    Code replaced

---
+++ src/test/java/com/itechart/certificates/gift/task5/ExtendedGiftCertificatesTask5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.certificates.gift.task5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendedGiftCertificatesTask5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
