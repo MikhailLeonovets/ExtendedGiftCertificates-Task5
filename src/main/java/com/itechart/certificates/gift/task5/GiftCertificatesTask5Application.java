f54176ab333c178dacde740c1ca0196ab3432034
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-06-28 17:30:06 +0300

    database connection properties added

--- src/main/java/com/itechart/certificates/gift/task5/GiftCertificatesTask5Application.java
+++ src/main/java/com/itechart/certificates/gift/task5/GiftCertificatesTask5Application.java
@@ -1,13 +1,11 @@
-package com.itechart.giftcertificates.task5.GiftCertificatesTask5;
+package com.itechart.certificates.gift.task5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GiftCertificatesTask5Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GiftCertificatesTask5Application.class, args);
 	}
-
 }
---
+++ src/main/java/com/itechart/certificates/gift/task5/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.itechart.certificates.gift.task5.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository {
+}
