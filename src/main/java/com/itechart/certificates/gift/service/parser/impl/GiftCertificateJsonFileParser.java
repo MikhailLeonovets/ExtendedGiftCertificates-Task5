f7d73e1a87f516b5e887679249ef543ec797b2e8
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-07-06 13:51:12 +0300

    GiftCertificateJsonFileParser updated

--- src/main/java/com/itechart/certificates/gift/service/parser/impl/GiftCertificateJsonFileParser.java
+++ src/main/java/com/itechart/certificates/gift/service/parser/impl/GiftCertificateJsonFileParser.java
@@ -5,15 +5,13 @@ import com.itechart.certificates.gift.service.exception.DataInputException;
 import com.itechart.certificates.gift.service.exception.IncorrectEntityFileException;
 import com.itechart.certificates.gift.service.model.GiftCertificateAndItsTags;
 import com.itechart.certificates.gift.service.parser.EntityFileParser;
-import net.minidev.json.JSONObject;
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static com.itechart.certificates.gift.service.exception.constant.ExceptionMessage.INCORRECT_ENTITY_FILE_MSG;
 import static com.itechart.certificates.gift.service.exception.constant.ExceptionMessage.INCORRECT_INPUT_MSG;
 import static com.itechart.certificates.gift.service.exception.constant.ExceptionMessage.NULL_FILE_MSG;
 
@@ -26,12 +24,12 @@ public class GiftCertificateJsonFileParser implements EntityFileParser {
 		}
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
-			new JSONObject(FileUtils.readFileToString(file, StandardCharsets.UTF_8));
-			objectMapper.readTree(file);
 			return objectMapper.readValue(file, objectMapper.getTypeFactory().constructCollectionType(List.class,
 					GiftCertificateAndItsTags.class));
 		} catch (IOException e) {
 			throw new DataInputException(INCORRECT_INPUT_MSG);
+		} catch (Exception e) {
+			throw new IncorrectEntityFileException(INCORRECT_ENTITY_FILE_MSG);
 		}
 	}
 }
